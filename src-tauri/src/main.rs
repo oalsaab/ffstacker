823092ede2effe944c5e4f321858da454e068b40
Omar Al-Saab <omar.al-saab@creditsafe.com>
2024-09-08 00:20:20 +0100

    Linking Tauri with frontent & updates to grid with add, reset & process buttons

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone)]
+struct Item {
+    id: String,
+    x: u8,
+    y: u8,
+    content: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn process_stack(stack: Vec<Item>) -> String {
+    let item = stack.first();
+
+    let result = match item {
+        Some(item) => item.id.clone(),
+        None => String::from("No value!"),
+    };
+
+    format!("From rust, first ID: {}", result)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![process_stack])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect, useRef, createRef } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 import "gridstack/dist/gridstack.min.css";
+import "gridstack/dist/gridstack-extra.min.css";
 import { GridStack } from "gridstack";
 
 // Define the Item component that accepts id as a prop
@@ -16,12 +16,14 @@ const Item: React.FC<ItemProps> = ({ id }) => <div>{id}</div>;
 interface ControlledStackProps {
   items: Array<{ id: string }>;
   addItem: () => void;
+  resetItems: () => void;
 }
 
 // ControlledStack component
 const ControlledStack: React.FC<ControlledStackProps> = ({
   items,
   addItem,
+  resetItems,
 }) => {
   const refs = useRef<{ [key: string]: React.RefObject<HTMLDivElement> }>({});
   const gridRef = useRef<GridStack | null>(null);
@@ -37,31 +39,51 @@ const ControlledStack: React.FC<ControlledStackProps> = ({
   useEffect(() => {
     // Initialize GridStack only once
     gridRef.current =
-      gridRef.current || GridStack.init({ float: false }, ".controlled");
+      gridRef.current ||
+      GridStack.init(
+        { float: false, disableResize: true, column: 6 },
+        ".controlled"
+      );
 
     const grid = gridRef.current;
+    grid.compact("compact");
 
     grid.batchUpdate();
     grid.removeAll(false); // Clear existing widgets
 
     items.forEach(({ id }) => {
       if (refs.current[id]?.current) {
-        grid.makeWidget(refs.current[id].current!); // Add new widgets
+        grid.makeWidget(refs.current[id].current!, { id: id }); // Add new widgets
       }
     });
 
     grid.batchUpdate(false);
   }, [items]);
 
+  const processStack = async () => {
+    const layout = gridRef.current?.save();
+
+    if (layout) {
+      console.log(layout);
+      let r = await invoke("process_stack", { stack: layout });
+      console.log(r);
+    }
+  };
+
   return (
-    <div>
-      <button onClick={addItem}>Add new widget</button>
-      <div className="grid-stack controlled">
+    <div className="controlled-container">
+      <div className="top-button-container">
+        <button onClick={addItem}>Add</button>
+        <button onClick={resetItems}>Reset</button>
+        <button onClick={processStack}>Process</button>
+      </div>
+      <div className="grid-container grid-stack controlled">
         {items.map((item) => (
           <div
             ref={refs.current[item.id]}
             key={item.id}
             className="grid-stack-item"
+            data-id={item.id}
           >
             <div className="grid-stack-item-content">
               <Item {...item} />
@@ -75,13 +97,19 @@ const ControlledStack: React.FC<ControlledStackProps> = ({
 
 // ControlledExample component
 const ControlledExample: React.FC = () => {
-  const [items, setItems] = useState([{ id: "item-1" }, { id: "item-2" }]);
+  const initialItems = [{ id: "1" }, { id: "2" }];
+  const [items, setItems] = useState(initialItems);
+
+  const addItem = () => {
+    setItems([...items, { id: `${items.length + 1}` }]);
+  };
+
+  const resetItems = () => {
+    setItems(initialItems);
+  };
 
   return (
-    <ControlledStack
-      items={items}
-      addItem={() => setItems([...items, { id: `item-${items.length + 1}` }])}
-    />
+    <ControlledStack items={items} addItem={addItem} resetItems={resetItems} />
   );
 };
 
